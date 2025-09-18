cc9d1d4901b750bfdb4a3639755c401bd45e7414
cao <codeing0085@gmail.com>
2025-09-18 23:16:57 +0800

    ds

--- client.config.js
+++ client.config.js
@@ -1,11 +1,25 @@
 const WebContentsView_Config = [
-    { key: 'SIDEBAR', instanceId:'default', show: true, buttonName: '' },
-    { key: 'EDITOR', instanceId:'default',show: true, buttonName: '編輯器'},
-    { key: 'PREVIEWER', instanceId: '1', show: false, buttonName: '預覽1' },
-    { key: 'PREVIEWER', instanceId: '2', show: false, buttonName: '預覽2' },
+  { key: 'SIDEBAR', instanceId:'default', show: true},
+  { key: 'EDITOR', instanceId:'default',show: true, buttonName: '編輯器'},
+  { key: 'PREVIEWER', instanceId: '1', show: false, buttonName: '預覽1' },
+  { key: 'PREVIEWER', instanceId: '2', show: false, buttonName: '預覽2' },
 ];
+
+const Client_Env = {
+  Account: "User00123",
+  Password: "321321",
+  API: "/",
+  Project: "./webProject/xxxxx",
+  Folder: "/"
+};
+
+const Deploy_Env = {
+
+};
 
 
 module.exports = {
-  WebContentsView_Config
+  WebContentsView_Config,
+  Client_Env,
+  Deploy_Env
 };
